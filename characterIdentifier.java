b91b4f7b8c9652d8c6ced496b0fc8c736cad2df7
Hamboger <120651600+Hamboger@users.noreply.github.com>
2022-12-16 22:42:51 +0800

    Add files via upload

--- characterIdentifier.java
+++ characterIdentifier.java
@@ -4,6 +4,8 @@ public class characterIdentifier{
    public static void main(String[] args){
       Scanner con = new Scanner(System.in);
       String characters = "";
+      System.out.println("Enter a String: ");
+      characters = con.nextLine();
       int lowerCaseCount = 0, lowerCaseVowelCount = 0, lowerCaseVowelConsonant = 0, specialChar = 0;
       char[] smallLetters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
       char[] smallLetterVowels = {'a','e','i','o','u'};
